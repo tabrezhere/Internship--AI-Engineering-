ec3d0aa838385c244955a93be3d67cabf3fb69ca
Tabrez Ahmed <tabrezhere@gmail.com>
2025-12-17 19:24:32 +0530

    Create Azure_CodingAssistant_chat.py

---
+++ 3-Azure_Openai_Chat/Azure_CodingAssistant_chat.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+from openai import AzureOpenAI
+load_dotenv(override=True)
+
+client = AzureOpenAI(
+    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+    api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
+)
+
+models = client.models.list()
+
+messages = [
+    {"role": "system", "content": "You are a coding mentor"},
+    {"role": "user", "content": "Explain linked list with example"}
+]
+
+response = client.chat.completions.create(
+    model=os.getenv("AZURE_OPENAI_DEPLOYMENT"),
+    messages=messages
+)
+
+print(response.choices[0].message.content)
