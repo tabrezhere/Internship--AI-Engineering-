eb6e478ba9b9b51b64660d41e3836e1a02ec7ceb
Tabrez Ahmed <tabrezhere@gmail.com>
2025-12-17 19:16:21 +0530

    Update testconnection.py

--- 2- First Azure OpenAI Call/testconnection.py
+++ 2- First Azure OpenAI Call/testconnection.py
@@ -1,10 +1,18 @@
+import os
+from dotenv import load_dotenv
 from openai import AzureOpenAI
+load_dotenv(override=True)
+
+print("KEY:", os.getenv("AZURE_OPENAI_API_KEY"))
+print("ENDPOINT:", os.getenv("AZURE_OPENAI_ENDPOINT"))
+print("DEPLOYMENT:", os.getenv("AZURE_OPENAI_DEPLOYMENT"))
+
+
 
 client = AzureOpenAI(
-    api_key="",
-    azure_endpoint="",
-    api_version="",
+    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+    api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
 )
-
 models = client.models.list()
 print("CONNECTED")
