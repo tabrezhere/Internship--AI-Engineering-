3e25311953de3eccffce1a947b12150abf785979
Tabrez Ahmed <tabrezhere@gmail.com>
2025-12-17 19:27:08 +0530

    Update and rename azure-openai-chat.py to azure-openai-HelpChat.py

--- 3-Azure_Openai_Chat/azure-openai-HelpChat.py
+++ 3-Azure_Openai_Chat/azure-openai-HelpChat.py
@@ -1,13 +1,12 @@
 import os
 from dotenv import load_dotenv
 from openai import AzureOpenAI
-
-load_dotenv()
+load_dotenv(override=True)
 
 client = AzureOpenAI(
-    api_key="",
-    azure_endpoint="",
-    api_version="",
+    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+    api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
 )
 
 response = client.chat.completions.create(
